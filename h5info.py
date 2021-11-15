12b7ae5efa664c983a7b0e47a8277dbd7f44b2b4
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2021-11-15 14:18:46 +0000

    Added colours to add readability

--- h5info.py
+++ h5info.py
@@ -8,28 +8,28 @@ import h5py
 
 def print_dataset(slice):
     if len(slice.shape) >= 3:
-        print("\nVOLUME")
+        print("\033[39m\nVOLUME")
         for i in range(len(slice)):
-            print(f'SLICE {i+1}')
+            print(f'\033[39mSLICE {i+1}')
             print_dataset(slice[i])
     elif len(slice.shape) == 2:
-        print(slice)
+        print(f'\33[32m{slice}')
     elif len(slice.shape) == 0:
-        print(slice[()])
+        print(f'\33[32m{slice[()]}')
     else:
-        print(slice[0:])
+        print(f'\33[32m{slice[0:]}')
 
 def print_metadata(h5_object):
-    print('attributes: ', list(h5_object.attrs.items()), '\n')
+    print('\033[36mattributes: ', list(h5_object.attrs.items()), '\n')
 
 
 def print_dataset_info(dataset):
-    print(dataset.name, 'is a dataset!')
+    print(f'{dataset.name}, is a dataset!')
     shape = dataset.shape
-    print('shape: ', shape)
+    print('\033[36mshape: ', shape)
     print('data type: ', dataset.dtype)
     print_metadata(dataset)
-    u_input = input("Type 'v' to view the data, or enter to continue: ")
+    u_input = input("\33[39mType 'v' to view the data, or enter to continue: ")
     if u_input == 'v':
         print_dataset(dataset)
         print()
@@ -40,7 +40,7 @@ def print_groups(group):
         print_dataset_info(group)
         return 'dataset'
     else:
-        print(f'In {group.name} :')
+        print(f'\033[0;39mIn {group.name} :')
         print(f'{list(group.keys())}\n')
         return 'group'
 
@@ -50,7 +50,7 @@ def groups(group):
         type = print_groups(group)
         if type == 'dataset':
             return 'b'
-        u_input = input("Type name of next group/dataset, 'a' to view attributes (metadata) of current group, 'b' to go back 1 layer, or 'exit' to exit:\n")
+        u_input = input("\033[39mType name of next group/dataset, 'a' to view attributes (metadata) of current group, 'b' to go back 1 layer, or 'exit' to exit:\n")
         if u_input == 'b' or u_input == 'exit':
             return u_input
         elif u_input == 'a':
@@ -69,7 +69,7 @@ def groups(group):
 
 
 while True:
-    file_name = input("Hi there! Enter a hdf5 file to get some info on it, or type 'quit' to quit the program:\n")
+    file_name = input("\033[39mHi there! Enter a hdf5 file to get some info on it, or type 'quit' to quit the program:\n")
     if file_name == 'quit':
         break
     try:
